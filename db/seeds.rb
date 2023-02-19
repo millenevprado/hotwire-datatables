a1eed0bf00bb28f07826199bf810c2264b7de94f
Millene Prado <millene_vprado@hotmail.com>
2023-02-19 16:00:10 -0300

    generated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning db..."
+Employee.destroy_all
+
+puts "Creating employees..."
+10_000.times do
+  Employee.create(
+    name: Faker::Name.name,
+    position: ["Accountat", "Chief Executive Officer (CEO)", "Chief Financial Officer (CFO)", "Engineer"].sample,
+    office: ["London", "Tokyo", "New York", "Singapore"].sample,
+    age: rand(20..70),
+    start_date: rand(1..1000).days.ago.to_date
+  )
+end
+
+puts "#{Employee.count} employees created!"
+puts "Finished!"
