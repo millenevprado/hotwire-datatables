06b8bf7ecd87bef073f9831207273635e60d1b0a
Millene Prado <millene_vprado@hotmail.com>
2023-02-19 16:13:40 -0300

    added employee index as root and removed pages contoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   resources :employees
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "employees#index"
 end
